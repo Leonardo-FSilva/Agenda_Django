824f83ed1f929ea83c1156ecb29d76a53acf0f70
Leonardo Felipe da Silva <leonardo_felipe_silva@outlook.com>
2024-09-16 22:01:51 -0300

    course update

--- contact/urls.py
+++ contact/urls.py
@@ -5,5 +5,10 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('',views.index, name='index'),
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
 ]
+
