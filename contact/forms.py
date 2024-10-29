520214831063e98ef1c2bedd3f9f210744f701cb
Leonardo Felipe da Silva <leonardo_felipe_silva@outlook.com>
2024-10-29 17:47:47 -0300

    novos arquivos

---
+++ contact/forms.py
@@ -0,0 +1,72 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+
+from . import models
+
+
+class ContactForm(forms.ModelForm):
+
+    picture = forms.ImageField(widget=forms.FileInput(attrs={'accept':'image/*'}))
+
+    class Meta:
+
+        model = models.Contact
+        fields = ('first_name','last_name','phone','email','description','category','picture')
+
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Veio do add_error',
+                    code='invalid'
+                )
+            )
+
+        return first_name
+ 
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=5,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name','last_name', 'email',
+            'username', 'password1', 'password2'
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(email, 'This email already exists.', code='invalid')
+
+        return email
--- contact/urls.py
+++ contact/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('search/', views.search, name='search'),
 
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/detail/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/detail/delete/', views.delete, name='delete'),
     path('contact/create/', views.create, name='create'),
+
+    path('user/create/', views.register, name='register'),
 ]
 
